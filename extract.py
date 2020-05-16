efa19882017ad875a5c694a4957f6a69fcbc0aab
mkisantal <kisantal.mate@gmail.com>
2020-05-16 06:51:24 +0000

    formatting and minor loading change

--- extract.py
+++ extract.py
@@ -4,7 +4,6 @@ import numpy as np
 from PIL import Image
 
 
-
 # path and names
 img_dir = '/home/ubuntu/data/Screenshots/'
 img_name_list = sorted(os.listdir(img_dir))
@@ -41,29 +40,32 @@ def ocr(crop_dict):
                 ocr_result = '1'
         ocr_results[key] = ocr_result
     return ocr_results
-        
+   
+    
 def print_results(ocr_results):
     for key in TARGETS:
         print('{}: {}'.format(key, ocr_results[key]))
     print('--'*5)
     return
 
-def load(img_name):
-    pil_img = Image.open(os.path.join(img_dir, img_name))
+
+def load(img_path):
+    pil_img = Image.open(img_path)
     np_img = np.array(pil_img)
     return np_img
-
-              
+             
              
 def main():
     print('started')
     for img_name in img_name_list:
         print(img_name)
-        np_img = load(img_name)
+        img_path = os.path.join(img_dir, img_name)
+        np_img = load(img_path)
         crops = extract_crops(np_img)
         results = ocr(crops)
         print_results(results)
-        
+
+
 if __name__ == '__main__':
     main()
 
